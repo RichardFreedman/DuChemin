eff7156aa5efad05b60144162c00ceaf63cfad9c
Micah Walter <mwalter@haverford.edu>
2014-07-02 10:56:31 -0400

    Updates for viewing notation from piece view

--- duchemin/static/js/notation.js
+++ duchemin/static/js/notation.js
@@ -1,24 +1,7 @@
-
-
-function attachAnalysisClickEvents() {
-    $('.view-analysis').on({
-        'click': function(event) {
-            $('#analysis-modal').remove();
-            modal = $("<div />", {
-                "id": "analysis-modal"
-            }).appendTo("body");
-
-            $("#analysis-modal").dialog({
-                'height': 500,
-                'width': 920,
-                'modal': true,
-                'title': 'Analysis View'
-            });
-
-            ajaxRenderAnalysis($(this).attr('anid'));
-            return false; // prevent the page from jumping when the link is clicked
-        }
-    });
+function log(msg) {
+    setTimeout(function() {
+        throw new Error(msg);
+    }, 0);
 }
 
 function attachPhraseClickEvents() {
@@ -30,54 +13,94 @@ function attachPhraseClickEvents() {
             }).appendTo("body");
 
             $("#analysis-modal").dialog({
-                'height': 500,
-                'width': 920,
+                'height': 600,
+                'width': 980,
                 'modal': true,
-                'title': 'Phrase View'
+                'title': $(this).data('pieceid') + ', phrase ' + $(this).data('phrasenum')
             });
-
-            ajaxRenderPhrase($(this).attr('phid'));
+            $("<div />", {
+                "class": "analysis-modal-body"
+            }).appendTo(modal);
+            ajaxRenderAnalysis($(this).attr('phid'), $(this).data('pieceid'),
+                $(this).data('start'), $(this).data('stop'), true);
             return false;
         }
     });
 }
 
-function ajaxRenderPhrase(pid) {
-    $.ajax({
-        url: '/data/phrase/' + pid,
-        dataType: 'json',
-        success: function(data, status, xhr) {
-            var modal = $("#analysis-modal");
+function attachAnalysisClickEvents() {
+    $('.view-analysis').on({
+        'click': function(event) {
+            $("#analysis-modal").remove();
+            var modal = $("<div />", {
+                "id": "analysis-modal"
+            }).appendTo("body");
 
+            $("#analysis-modal").dialog({
+                'height': 600,
+                'width': 980,
+                'modal': true,
+                'title': ($(this).data('pieceid') + ', measures ' +
+                    $(this).data('start') + '–' + $(this).data('stop'))
+            });
             $("<div />", {
-                "id": "analysis-modal-body"
+                "class": "analysis-modal-body"
             }).appendTo(modal);
-
-            $("#analysis-modal-body").append(data['music']);
-
-            var MEI = $("#meiScore");
-            var cv = $('div#music canvas')[0];
-            render_notation(MEI, cv, data['dimensions'][0], data['dimensions'][1]);
+            ajaxRenderAnalysis($(this).attr('anid'), $(this).data('pieceid'),
+                $(this).data('start'), $(this).data('stop'), false);
+            return false;
         }
     });
 }
 
-function ajaxRenderAnalysis(anid) {
+function ajaxRenderAnalysis(id, piece_id, start, end, phrase) {
     $.ajax({
-        url: '/data/analysis/' + anid,
+        url: (phrase ? '/data/phrase/' : '/data/analysis/')  + id,
         dataType: 'json',
         success: function(data, status, xhr) {
+            var loadedXML = meiView.Util.loadXMLDoc('/static/xml/' + piece_id + '.xml');
+            var filteredXml = meiView.filterMei(loadedXML, { noSysBreak:true });
+            var meiDoc = new MeiLib.MeiDoc(filteredXml);
+
+            var pagination = new meiView.Pages();
+
+            /* If you want scrolling: */
+            pagination.AddPage(start, end);
+
+            /* If you want pagination rather than scrolling:
+
+            // Throughout, -1 appears because measures are not counted "logically"
+            var max_m = start;
+
+            while (max_m < end) {
+                if (end - (max_m - 1) < 8) {
+                    // Less than 8 measures left: fit it all in
+                    pagination.AddPage(max_m, end);
+                    max_m = end;
+                }
+                else if (end - (max_m - 1) < 12) {
+                    // To avoid uneven cramming at the end, consider pages of 5
+                    pagination.AddPage(max_m, max_m + 4);
+                    max_m += 5;
+                }
+                else {
+                    pagination.AddPage(max_m, max_m + 3);
+                    max_m += 4;
+                }
+            }
+            */
+
+            var modal_viewer = new meiView.CompactViewer({
+                maindiv: $('.analysis-modal-body'),
+                MEI: meiDoc,
+                pages: pagination,
+                title: "",
+                displayFirstPage: true,
+                scale: 0.85,
+                pxpMeasure: 200,
+            });
+
             var modal = $("#analysis-modal");
-
-            $("<div />", {
-                "id": "analysis-modal-body"
-            }).appendTo(modal);
-
-            $("#analysis-modal-body").append(data['music']);
-
-            var MEI = $("#meiScore");
-            var cv = $('div#music canvas')[0];
-            render_notation(MEI, cv, data['dimensions'][0], data['dimensions'][1]);
         }
     });
 }
