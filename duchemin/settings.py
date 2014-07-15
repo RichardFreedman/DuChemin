0ec909e1a365a1580b8179f2ee9ea4dab749cba6
Micah Walter <micahjwalter@gmail.com>
2014-07-15 17:26:12 -0400

    Allow trailing slashes in URLs

--- duchemin/settings.py
+++ duchemin/settings.py
@@ -47,6 +47,9 @@ SHORT_DATETIME_FORMAT = 'Y-m-d, H:m'
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Make it acceptable for users to leave off a slash in URLs
+APPEND_SLASH = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'uploads/')
