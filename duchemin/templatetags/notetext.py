57af3185879874a59f78c9503e2ed9e4cf6497e1
Micah Walter <micahjwalter@gmail.com>
2014-07-18 11:03:32 -0400

    Corrected outdated comment text on notetext templatetag

--- duchemin/templatetags/notetext.py
+++ duchemin/templatetags/notetext.py
@@ -3,10 +3,10 @@ from django.template.defaultfilters import register
 
 @register.filter(name='notetext')
 def notetext(user, piece):
-	# Returns True iff the user has a note on a given piece.
+    # Returns the text of a note, if there is one; else return empty string
     text = ''
     for note in user.notes.all().order_by("updated"):
-    	# We will overwrite with most recent if there is more than one
+        # We will overwrite with most recent if there is more than one
         if note.piece == piece:
             text = note.text
     return text
