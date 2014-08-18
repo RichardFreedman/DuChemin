e2dbb9b2eeb13cb25deb56f36c36e259bb3df2ba
Micah Walter <micahjwalter@gmail.com>
2014-08-18 14:23:13 -0400

    Revised serializer for phrase that gives actual piece_id

--- duchemin/serializers/phrase.py
+++ duchemin/serializers/phrase.py
@@ -1,7 +1,19 @@
 from duchemin.models.phrase import DCPhrase
+from duchemin.models.piece import DCPiece
 from rest_framework import serializers
 
+
+class DCPiecePhraseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DCPiece
+        fields = ('piece_id',)
+
+
 class DCPhraseSerializer(serializers.HyperlinkedModelSerializer):
+    piece = DCPiecePhraseSerializer()
+
     class Meta:
         model = DCPhrase
-        # fields = ('url',)
+        ('url', 'piece_id', 'phrase_num', 'text',
+         'phrase_start', 'phrase_stop',
+         'phrase_text', 'rhyme',)
