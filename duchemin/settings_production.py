b9450181c7b9efb44ff698799a82de3bd1e5f10d
Micah Walter <micahjwalter@gmail.com>
2014-08-18 16:20:01 -0400

    Attempting good settings_production.py file

--- duchemin/settings_production.py
+++ duchemin/settings_production.py
@@ -1,11 +1,11 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'duchemin',                      # Or path to database file if using sqlite3.
-        'USER': 'duchemin',                      # Not used with sqlite3.
-        'PASSWORD': 'SecurePassword',                  # Not used with sqlite3.
+        'NAME': 'duchemin_webapp',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
