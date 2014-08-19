7f9ce8c00045ebd7a4da2c429c94c1bf55341c35
Micah Walter <micahjwalter@gmail.com>
2014-08-19 09:26:58 -0400

    Management command: add mp3 for books 1-11, mei for all

--- duchemin/management/commands/add_urls.py
+++ duchemin/management/commands/add_urls.py
@@ -13,7 +13,18 @@ class Command(BaseCommand):
         for piece in DCPiece.objects.all():
             try:
                 piece.mei_link = MEI_PATH + piece.piece_id + '.xml'
-                piece.audio_link = MP3_PATH + piece.piece_id + '.mp3'
+                if ('DC01' in piece.piece_id or
+                        'DC02' in piece.piece_id or
+                        'DC03' in piece.piece_id or
+                        'DC04' in piece.piece_id or
+                        'DC05' in piece.piece_id or
+                        'DC06' in piece.piece_id or
+                        'DC07' in piece.piece_id or
+                        'DC08' in piece.piece_id or
+                        'DC09' in piece.piece_id or
+                        'DC10' in piece.piece_id or
+                        'DC11' in piece.piece_id):
+                    piece.audio_link = MP3_PATH + piece.piece_id + '.mp3'
                 piece.save()
             except:
                 e = str(sys.exc_info())
