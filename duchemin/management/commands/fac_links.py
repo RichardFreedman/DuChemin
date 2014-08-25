fb219d457ed892d30a6edead9cb24db64e3cf0e0
Micah Walter <micahjwalter@gmail.com>
2014-08-25 17:17:10 -0400

    Completed command for facsimile links

--- duchemin/management/commands/fac_links.py
+++ duchemin/management/commands/fac_links.py
@@ -6,13 +6,17 @@ import sys
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        codes = { 1: '149',   2: '140',   3: '141',   4: '142',
+                  5: '151',   6: '152',   7: '153',   8: '154',
+                  9: '165',  10: '170',  11: '193',  12: '228',
+                 13: '229',  14: '261',  15: '262',  16: '323',
+                 }
         for book in DCBook.objects.all():
             try:
-                if book.book_id == 1:
-                    book.cesr = 'http://ricercar.cesr.univ-tours.fr/3-programmes/EMN/duchemin/pages/consult.asp?table=A149'
-                    book.save()
-                else:
-                    self.stdout.write(str(book.book_id) + '\n', ending='')
+                book.cesr = ('http://ricercar.cesr.univ-tours.fr/3-programmes/EMN/duchemin/pages/consult.asp?table=A' +
+                                 codes[book.book_id]
+                                 )
+                book.save()
             except:
                 e = str(sys.exc_info())
                 self.stdout.write('%s\n' % e, ending='')
