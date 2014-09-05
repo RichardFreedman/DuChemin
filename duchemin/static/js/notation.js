8e5c34f89f92bde4a70e5a1e6f5192bd9c46baf5
Micah Walter <micahjwalter@gmail.com>
2014-09-04 21:32:05 -0400

    Improved: Remove unnecessary ajax calls and parameters from notation.

--- duchemin/static/js/notation.js
+++ duchemin/static/js/notation.js
@@ -35,7 +35,7 @@ function attachPhraseClickEvents() {
                 "class": "phrase-modal-body"
             }).appendTo(modal);
 
-            ajaxRenderPhrase($(this).attr((is_analysis ? 'anid' : 'phid')),
+            ajaxRenderPhrase(
                 $(this).data('meilink'),
                 $(this).data('start'),
                 $(this).data('stop'),
@@ -46,55 +46,49 @@ function attachPhraseClickEvents() {
     });
 }
 
-function ajaxRenderPhrase(id, mei_link, start, end, is_analysis) {
-    $.ajax({
-        url: (is_analysis ? '/data/analysis/' : '/data/phrase/') + id,
-        dataType: 'json',
-        success: function(data, status, xhr) {
-            var loadedXML = meiView.Util.loadXMLDoc(mei_link);
-            var filteredXml = meiView.filterMei(loadedXML, { noSysBreak:true });
-            var meiDoc = new MeiLib.MeiDoc(filteredXml);
+function ajaxRenderPhrase(mei_link, start, end, is_analysis) {
+    var loadedXML = meiView.Util.loadXMLDoc(mei_link);
+    var filteredXml = meiView.filterMei(loadedXML, { noSysBreak:true });
+    var meiDoc = new MeiLib.MeiDoc(filteredXml);
 
-            var pagination = new meiView.Pages();
+    var pagination = new meiView.Pages();
 
-            /* If you want scrolling: */
-            pagination.AddPage(start, end);
+    /* If you want scrolling: */
+    pagination.AddPage(start, end);
 
-            /* If you want pagination rather than scrolling:
+    /* If you want pagination rather than scrolling:
 
-            // Throughout, -1 appears because measures are not counted "logically"
-            var max_m = start;
+    // Throughout, -1 appears because measures are not counted "logically"
+    var max_m = start;
 
-            while (max_m < end) {
-                if (end - (max_m - 1) < 8) {
-                    // Less than 8 measures left: fit it all in
-                    pagination.AddPage(max_m, end);
-                    max_m = end;
-                }
-                else if (end - (max_m - 1) < 12) {
-                    // To avoid uneven cramming at the end, consider pages of 5
-                    pagination.AddPage(max_m, max_m + 4);
-                    max_m += 5;
-                }
-                else {
-                    pagination.AddPage(max_m, max_m + 3);
-                    max_m += 4;
-                }
-            }
-            */
-
-            var modal_viewer = new meiView.CompactViewer({
-                maindiv: $('.phrase-modal-body'),
-                MEI: meiDoc,
-                pages: pagination,
-                title: "",
-                displayFirstPage: true,
-                scale: 0.8,
-                mode: meiView.Mode.SINGLE_PAGE,
-                pxpMeasure: 280,
-            });
-
-            var modal = $("#phrase-modal");
+    while (max_m < end) {
+        if (end - (max_m - 1) < 8) {
+            // Less than 8 measures left: fit it all in
+            pagination.AddPage(max_m, end);
+            max_m = end;
         }
+        else if (end - (max_m - 1) < 12) {
+            // To avoid uneven cramming at the end, consider pages of 5
+            pagination.AddPage(max_m, max_m + 4);
+            max_m += 5;
+        }
+        else {
+            pagination.AddPage(max_m, max_m + 3);
+            max_m += 4;
+        }
+    }
+    */
+
+    var modal_viewer = new meiView.CompactViewer({
+        maindiv: $('.phrase-modal-body'),
+        MEI: meiDoc,
+        pages: pagination,
+        title: "",
+        displayFirstPage: true,
+        scale: 0.8,
+        mode: meiView.Mode.SINGLE_PAGE,
+        pxpMeasure: 280,
     });
+
+    var modal = $("#phrase-modal");
 }
