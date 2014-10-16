2b1ad9abbad191420b1a67d987503597dea0ab2d
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-10-16 13:31:07 -0400

    Revert commented-out solr server.

--- duchemin/settings_production.py
+++ duchemin/settings_production.py
@@ -10,5 +10,5 @@ DATABASES = {
 }
 
 #SOLR_SERVER = "http://duchemin-dev.haverford.edu:8080/duchemin-solr"
-# SOLR_SERVER = "http://localhost:8080/duchemin-solr/"
+SOLR_SERVER = "http://localhost:8080/duchemin-solr/"
 # VEXF_SERVER = "http://localhost:8080/notation"
