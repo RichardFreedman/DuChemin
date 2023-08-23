9248880969ad80f6734f5aa40f0cf5473efbbb9c
eleon024 <eleon@haverford.edu>
2023-08-22 22:41:48 -0500

    more improvements

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,24 +3,24 @@
 set -e
 
 # Start Solr service using the provided command
-cd /app/solr/
-mvn tomcat:run-war &
+# cd /app/solr/
+# mvn tomcat:run-war &
 
 
 # Wait for Solr to start (adjust this delay as needed)
-sleep 10
-cd ..
+# sleep 10
+# cd ..
 python manage.py makemigrations
 python manage.py migrate
 # Apply migrations and run the Django development server
-python manage.py loaddata /app/duchemin/data/duchemin_dcperson.json
-python manage.py loaddata /app/duchemin/data/duchemin_dcbook.json
-python manage.py loaddata /app/duchemin/data/duchemin_dccontentblock.json
-python manage.py loaddata /app/duchemin/data/duchemin_dcpiece.json
-python manage.py loaddata /app/duchemin/data/duchemin_dcreconstruction.json
-python manage.py loaddata /app/duchemin/data/duchemin_dcphrase.json
-python manage.py loaddata /app/duchemin/data/duchemin_dcanalysis.json
-python manage.py loaddata /app/duchemin/data/duchemin_dcnote.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcperson.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcbook.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dccontentblock.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcpiece.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcreconstruction.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcphrase.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcanalysis.json
+# python manage.py loaddata /app/duchemin/data/duchemin_dcnote.json
 
 
 #python manage.py runserver 0.0.0.0:8000
