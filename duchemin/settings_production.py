29f67357d3cae0592a969b171115a9e17c62ec54
Richard Freedman <rfreedma@haverford.edu>
2024-10-23 16:25:28 -0400

    update settings production to use the postgres names and pws

--- duchemin/settings_production.py
+++ duchemin/settings_production.py
@@ -1,4 +1,6 @@
 import os
+from decouple import config
+
 
 # Should be not be True for production
 DEBUG = False
@@ -17,9 +19,12 @@ DATABASES = {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE':   'django.db.backends.postgresql',
         # FIXME: These need to be changed from the default values and the username/password need to come from a secret (and synched with the values in docker-compose)
-        'NAME':     'postgres',
-        'USER':     'postgres',
-        'PASSWORD': 'postgres',
+        # 'NAME':     'postgres',
+        # 'USER':     'postgres',
+        # 'PASSWORD': 'postgres',
+        'NAME':     config('POSTGRES_DB'),
+        'USER':     config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST':     'db',                       # Set to empty string for localhost. Not used with sqlite3.
         'PORT':     '5432',                     # Set to empty string for default. Not used with sqlite3.
     }
